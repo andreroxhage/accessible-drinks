2934f686b82dd6d89d603ad20938850704fc0e72
Carl Rikner <rikner.carl@gmail.com>
2024-11-22 13:29:17 +0100

    Get categories

--- src/app/utils/database.ts
+++ src/app/utils/database.ts
@@ -1,6 +1,5 @@
 export type Category = {
     title:string;
-    drinks:Drink[];
 }
 export type Drink = {
     strDrink:string;
@@ -20,28 +19,29 @@ async function apiFetch<T>(path:string): Promise<T> {
     return await response.json() as T;
 }
 
-export async function getCategories(){
+export async function getCategories():Promise<Category[]>{
     var categories = await apiFetch<{data:{strCategory:string}[]}>('list.php?c=list');
 
-    return categories.data.map(async e => {
-        var drinks = await apiFetch<{data:Drink[]}>('filter.php?c=' + e.strCategory);
-        var cat:Category = {
-            title: e.strCategory,
-            drinks: drinks.data
-        };
-        return cat;
+    return categories.data.map(data => {
+        let category:Category = {
+            title:data.strCategory
+        }
+        return category;
     });
-    
 }
+
 export async function getDrinksByCategory(category:string) {
     return await apiFetch<{drinks:{strDrink:string, strDrinkThumb:string, idDrink:number}[]}>('filter.php?c=' + category);
 }
+
 export async function getIngredients(){
     return await apiFetch<{data:{strIngredient:string}[]}>('list.php?i=list');
 }
+
 export async function getDrinksByIngredient(ingredient:string) {
     return await apiFetch<{drinks:{strDrink:string, strDrinkThumb:string, idDrink:number}[]}>('filter.php?i=' + ingredient);
 }
+
 export async function getDrinkByName(name:string){
     return await apiFetch<{drink:{}[]}>('search.php?s=' + name);
 }
