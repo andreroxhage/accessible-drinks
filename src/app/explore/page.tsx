8028d864d824c46d9bdcc5f3247a0f33ce5e9392
Carl Rikner <rikner.carl@gmail.com>
2024-12-18 15:14:16 +0100

    Fixed explore

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -10,10 +10,15 @@ import { PlusIcon, MinusIcon } from '@heroicons/react/20/solid';
 
 import categories from '../categories.json';
 import spirits from '../spirits.json';
+import glasses from '../glass.json'
+import alcoholicJSON from '../alcoholic.json'
+
 import {
   getDrinksByCategory,
   getDrinksByIngredient,
   getDrinks,
+  getDrinksByAlcoholContent,
+  getDrinksByGlass,
 } from '../utils/database';
 import { Drink } from '../types';
 import SearchResult from '../components/SearchResults';
@@ -23,142 +28,135 @@ interface DrinkMap {
   [id: string]: Drink;
 }
 
-const sortOptions = [
-  { name: 'Alphabetical', key: 'strDrink', ascending: true },
-  { name: 'Alcoholic First', key: 'strAlcoholic', ascending: true },
-  { name: 'Category', key: 'strCategory', ascending: true },
-];
-
-export default function ExplorePage({
-  params,
-}: {
-  params: Promise<{ searchQuery: string }>;
-}) {
-  const [filteredResult, setFilteredResult] = useState<any[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [filters, setFilters] = useState<
-    {
-      id: string;
-      name: string;
-      options: { value: string; label: string; checked: boolean }[];
-    }[]
-  >([]);
-  const [selectedFilters, setSelectedFilters] = useState<
-    Record<string, string[]>
-  >({
-    category: [],
-    alcoholic: [],
-    spirit: [],
-  });
-  const searchResultsContainerRef = useRef<HTMLDivElement>(null);
+export default function ExplorePage({params,}: {params: Promise<{ searchQuery: string }>;}) {
+    const [filteredResult, setFilteredResult] = useState<any[]>([]);
+    const [alcoholics, setAlcoholics] = useState<string[]>(alcoholicJSON);
+    const [loading, setLoading] = useState(false);
+    const [filters, setFilters] = useState<{id: string; name: string; options: { value: string; label: string; checked: boolean }[];}[]>([]);
+    const [selectedFilters, setSelectedFilters] = useState<Record<string, string[]>>({alcoholic:alcoholicJSON, category: [], spirit: [], glass:[] });
+    const searchResultsContainerRef = useRef<HTMLDivElement>(null);
 
   // Set focus to the search results container when it is rendered
-  useEffect(() => {
-    if (searchResultsContainerRef.current) {
-      searchResultsContainerRef.current.tabIndex = -1;
-      searchResultsContainerRef.current.focus();
-    }
-  }, []);
-
-  useEffect(() => {
-    var categoryOptions = categories.map(value => {
-      return {
-        value,
-        label: value,
-        checked: false,
-      };
-    });
-
-    const spiritOptions = spirits.map(value => {
-      return {
-        value,
-        label: value,
-        checked: false,
-      };
-    });
-    var filtersList = [
-      {
-        id: 'category',
-        name: 'Category',
-        options: categoryOptions,
-      },
-    ];
-    filtersList.push({
-      id: 'spirit',
-      name: 'Spirit',
-      options: spiritOptions,
-    });
-    setFilters(filtersList);
-  }, []);
-
-  useEffect(() => {
-    const fetchByFilters = async () => {
-      setLoading(true);
-      var drinkList: DrinkMap = {};
-      var categoryIds: Set<string> = new Set<string>();
-      for (var option of selectedFilters['category']) {
-        var drinks = await getDrinksByCategory(option);
-        drinks.forEach(drink => {
-          categoryIds.add(drink.idDrink);
-          drinkList[drink.idDrink] = drink;
-        });
-      }
-
-      var spiritsIds: Set<string> = new Set<string>();
-
-      for (var option of selectedFilters['spirit']) {
-        var drinks = await getDrinksByIngredient(option);
-
-        drinks.forEach(drink => {
-          spiritsIds.add(drink.idDrink);
-          drinkList[drink.idDrink] = drink;
-        });
-      }
-
-      if (spiritsIds.size == 0) {
-        if (categoryIds.size == 0) {
-          var drinks: Drink[] = await getDrinks();
-          setFilteredResult(drinks);
-        } else {
-          setFilteredResult(
-            Array.from(categoryIds).map(id => {
-              return drinkList[id];
-            })
-          );
+    useEffect(() => {
+        if (searchResultsContainerRef.current) {
+        searchResultsContainerRef.current.tabIndex = -1;
+        searchResultsContainerRef.current.focus();
         }
-      } else if (categoryIds.size == 0) {
-        setFilteredResult(
-          Array.from(spiritsIds).map(id => {
-            return drinkList[id];
-          })
-        );
-      } else {
-        setFilteredResult(
-          Array.from(spiritsIds.intersection(categoryIds)).map(id => {
-            return drinkList[id];
-          })
-        );
-      }
+    }, []);
+    useEffect(() => {
+        // Initialize filters
+        var filtersList = [
+            {
+                id: 'category',
+                name: 'Category',
+                options: categories.sort((a,b) => a.localeCompare(b)).map(value => {
+                    return {
+                    value,
+                    label: value,
+                    checked: false,
+                    };
+                }),
+            },{
+                id: 'spirit',
+                name: 'Spirit',
+                options: spirits.sort((a,b) => a.localeCompare(b)).map(value => {
+                    return {
+                    value,
+                    label: value,
+                    checked: false,
+                    };
+                }),
+            },{
+                id:'glass',
+                name: 'Glass',
+                options: glasses.sort((a,b) => a.localeCompare(b)).map(value => {
+                    return {
+                        value,
+                        label: value,
+                        checked:false
+                    }
+                })
+            }
+        ];
+
+        setFilters(filtersList);
+    }, []);
+
+  useEffect(() => {
+    const removeDuplicates = (list:Drink[]) => {
+        var cleanList:Drink[] = [];
+        var ids:string[] = [];
+        for(var i of list){
+            if(!ids.includes(i.idDrink)){
+                cleanList.push(i);
+                ids.push(i.idDrink);
+            }
+        }
+        return cleanList;
+    }
+
+    const fetchByFilters = async () => {
+        setLoading(true);
+        
+        var alcResults:Drink[] = [];
+        var catResults:Drink[] = [];
+        var spirResults:Drink[] = [];
+        var glassResults:Drink[] = [];
+
+        for(var option of selectedFilters['alcoholic']){
+            var data = await getDrinksByAlcoholContent(option);
+            alcResults.push(...data);
+        }
+
+        if(selectedFilters['category'].length > 0){
+            for(var option of selectedFilters['category']){
+                var data = await getDrinksByCategory(option);
+                var ids = data.map(i => {return i.idDrink});
+                catResults.push(...alcResults.filter(i => ids.includes(i.idDrink)));
+            }
+        }
+        if(selectedFilters['spirit'].length > 0){
+            for(var option of selectedFilters['spirit']){
+                var data = await getDrinksByIngredient(option);
+                var ids = data.map(i => {return i.idDrink});
+                spirResults.push(...alcResults.filter(i => ids.includes(i.idDrink)));
+            }
+        }
+        if(selectedFilters['glass'].length > 0){
+            for(var option of selectedFilters['glass']){
+                var data = await getDrinksByGlass(option);
+                var ids = data.map(i => {return i.idDrink});
+                glassResults.push(...alcResults.filter(i => ids.includes(i.idDrink)));
+            }
+        }
+
+        var results = removeDuplicates(catResults.concat(spirResults.concat(glassResults)));
+        if(results.length > 0){
+            results.sort((a,b) => a.strDrink.localeCompare(b.strDrink));
+            setFilteredResult(results);
+        }else{
+            setFilteredResult(alcResults);
+        }
     };
 
     fetchByFilters();
     setLoading(false);
   }, [selectedFilters]);
 
-  if (loading) return <div className="h-screen"></div>;
+    if (loading) return <div className="h-screen"></div>;
 
-  function handleFilterChange(id: string, value: string): void {
-    setSelectedFilters(prev => {
-      const currentFilters = prev[id] || [];
-      const isChecked = currentFilters.includes(value);
-      return {
-        ...prev,
-        [id]: isChecked
-          ? currentFilters.filter(f => f !== value)
-          : [...currentFilters, value],
-      };
-    });
-  }
+    function handleFilterChange(id: string, value: string): void {
+        setSelectedFilters(prev => {
+        const currentFilters = prev[id] || [];
+        const isChecked = currentFilters.includes(value);
+        return {
+            ...prev,
+            [id]: isChecked
+            ? currentFilters.filter(f => f !== value)
+            : [...currentFilters, value],
+        };
+        });
+    }
 
   return (
     <main className="container mx-auto px-6 py-16 sm:py-12 lg:px-8 lg:py-16 min-h-screen">
@@ -170,80 +168,67 @@ export default function ExplorePage({
 
       <section className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4 pt-6">
         {/* Filters */}
-        <form
-          id="main-content"
-          className="col-span-4 lg:col-span-1"
-          role="search"
-          aria-label="Filter drinks"
-        >
-          {filters.map(section => (
-            <Disclosure
-              key={section.id}
-              as="div"
-              className="border border-gray-200 py-6 bg-white hover:bg-gray-50 rounded-lg p-4 mb-4"
-            >
-              <h3 className="-my-3 flow-root">
-                <DisclosureButton className="group flex w-full items-center justify-between py-3 text-sm text-gray-400 hover:text-gray-500">
-                  <span className="font-medium text-gray-900">
-                    {section.name}
-                  </span>
-                  <span className="ml-6 flex items-center">
-                    <PlusIcon
-                      aria-hidden="true"
-                      className="size-5 group-data-[open]:hidden"
-                    />
-                    <MinusIcon
-                      aria-hidden="true"
-                      className="size-5 [.group:not([data-open])_&]:hidden"
-                    />
-                  </span>
-                </DisclosureButton>
-              </h3>
-              <DisclosurePanel className="pt-6">
-                <div className="space-y-4">
-                  {section.options.map(option => (
-                    <div key={option.value} className="flex items-center">
-                      <input
-                        type="checkbox"
-                        id={option.value}
-                        checked={selectedFilters[section.id].includes(
-                          option.value
-                        )}
-                        onChange={() =>
-                          handleFilterChange(section.id, option.value)
-                        }
-                        className="size-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                      />
-                      <label
-                        htmlFor={option.value}
-                        className="ml-3 text-sm text-gray-600"
-                      >
-                        {option.label}
-                      </label>
+        <form className="col-span-4 lg:col-span-1" role="search" aria-label="Filter drinks">
+            <fieldset key="alcoholic">
+                <Disclosure defaultOpen={true} key={alcoholics[0]} as="div" className="border border-gray-200 py-6 bg-white hover:bg-gray-50 rounded-lg p-4 mb-4">
+                <h3>
+                    Alcoholic content
+                </h3>
+                    <DisclosurePanel className="pt-6">
+                        {alcoholics.map(option => {
+                            return <div key={option} className="flex items-center">
+                                <input onChange={() => handleFilterChange("alcoholic", option)} type='checkbox' defaultChecked={true} id={option} value={option} name="alcoholic-category" className="size-4 border-gray-300 text-indigo-600 focus:ring-indigo-500"/>
+                                <label className="ml-3 text-sm text-gray-600" htmlFor={option}>{option}</label><br></br>
+                            </div>
+                        })}
+                    </DisclosurePanel>
+                </Disclosure>
+            </fieldset>
+            {filters.map(section => (
+            <fieldset key={section.id}>
+                <Disclosure key={section.id} as="div" className="border border-gray-200 py-6 bg-white hover:bg-gray-50 rounded-lg p-4 mb-4">
+                <h3 className="-my-3 flow-root">
+                    <DisclosureButton className="group flex w-full items-center justify-between py-3 text-sm text-gray-400 hover:text-gray-500">
+                    <legend>
+                        <span className="font-medium text-gray-900">
+                            {section.name}
+                        </span>{selectedFilters[section.id].length == 0 ? <span className='ml-2'>
+                            Showing all
+                            </span>: <></>}
+                    </legend>
+                    <span className="ml-6 flex items-center">
+                        <PlusIcon aria-hidden="true" className="size-5 group-data-[open]:hidden" />
+                        <MinusIcon aria-hidden="true" className="size-5 [.group:not([data-open])_&]:hidden" />
+                    </span>
+                    </DisclosureButton>
+                </h3>
+                <DisclosurePanel className="pt-6">
+                    
+                    <div className="space-y-4">
+                    {section.options.map(option => (
+                        <div key={option.value} className="flex items-center">
+                        <input type="checkbox" id={option.value} checked={selectedFilters[section.id].includes(option.value)} onChange={() => handleFilterChange(section.id, option.value) } className="size-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"/>
+                        <label htmlFor={option.value} className="ml-3 text-sm text-gray-600">
+                            {option.label}
+                        </label>
+                        </div>
+                    ))}
                     </div>
-                  ))}
-                </div>
-              </DisclosurePanel>
-            </Disclosure>
+                </DisclosurePanel>
+                </Disclosure>
+            </fieldset>
           ))}
         </form>
 
         {/* Loading state */}
         {loading && (
-          <div
-            role="status"
-            aria-live="assertive"
-            className="col-span-4 h-screen flex items-center justify-center"
-          >
+          <div role="status" aria-live="assertive" className="col-span-4 h-screen flex items-center justify-center">
             <span className="sr-only">Loading search results...</span>
             <div className="loader" aria-hidden="true"></div>
           </div>
         )}
         {/* Results Grid */}
-        <div
-          ref={searchResultsContainerRef}
-          className="lg:col-span-3 col-span-4"
-        >
+        <div ref={searchResultsContainerRef} className="lg:col-span-3 col-span-4">
           {!loading && <SearchResult drinks={filteredResult} />}
         </div>
       </section>
